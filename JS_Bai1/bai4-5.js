61c692760d768858da244881487a42f476249005
al-khoanv <khoanv@allogical.com>
2021-10-13 22:32:48 +0700

    update: Update code can't run

--- JS_Bai1/bai4-5.js
+++ JS_Bai1/bai4-5.js
@@ -1,7 +1,9 @@
-var i = 3;
-if (i > 2) {
-    console.log('i lớn hơn 2');
-    break;
-} else {
-    console.log('i không lớn hơn 2');
+
+for (var i = 3; i < 10; i++) {
+    if (i > 2) {
+        console.log('i lớn hơn 2');
+        break;
+    } else {
+        console.log('i không lớn hơn 2');
+    }
 }
